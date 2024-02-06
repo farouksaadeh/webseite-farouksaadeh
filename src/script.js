9c029b70fc73ab130ed0a9cc3c003b906b1ccbf3
TAASAFA8 <FaroukIzzDaa@hotmail.com>
2024-02-06 17:29:45 +0100

    Added Nav Bar and portfolio.html

---
+++ src/script.js
@@ -0,0 +1,15 @@
+const $indicator = document.
+    querySelector('.indicator');
+const $links = document.querySelectorAll('li a');
+
+$links.forEach(($link) => {
+    $link.addEventListener(
+        'mousemove',
+        (e) => {
+            const { offsetLeft,
+            offsetWidth } = e.target;
+            $indicator.style.left = offsetLeft + 'px';
+            $indicator.style.width = offsetWidth + 'px';
+        }
+    );
+});
